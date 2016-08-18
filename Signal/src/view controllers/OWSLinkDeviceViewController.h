00174eebe97512ac2307fe82897ed80c629892f3
Michael Kirk <michael.john.kirk@gmail.com>
2016-08-18 19:47:21 -0400

    Provision device from  QRCode.
    NEEDS DESIGN - will likely have an intermediate "device manager" screen.
    
    * extracted QRCode Scanner into re-usable ViewController
    
    // FREEBIE

---
+++ Signal/src/view controllers/OWSLinkDeviceViewController.h
@@ -0,0 +1,13 @@
+//
+//  OWSLinkDeviceViewController.h
+//  Signal
+//
+//  Created by Michael Kirk on 8/18/16.
+//  Copyright © 2016 Open Whisper Systems. All rights reserved.
+//
+
+#import "OWSQRCodeScanningViewController.h"
+
+@interface OWSLinkDeviceViewController : OWSQRCodeScanningViewController
+
+@end
